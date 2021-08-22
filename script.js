4ca4e7f401cd1c8e015aace87fb04aa6f67d5585
Scott Breitenbach <scott.breitenbach@gmail.com>
2021-08-21 21:41:41 -0400

    updating the page when you submit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function myFunction(zipcode) {
-    alert("Your zip code is " + zipcode);
+    //alert("Your zip code is " + zipcode);
+    var target = document.getElementById("weatherResults");
+    target.innerHTML += "<br>Your zip code is " + zipcode;
 }
