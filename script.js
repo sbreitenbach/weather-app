3b99b9ede665fa0c712e44488b5d6b27899358eb
Scott Breitenbach <scott.breitenbach@gmail.com>
2021-08-27 21:36:44 -0400

    adding call for geocode

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+function get_geocode(zipcode) {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "JSESSIONID=061C62770675DB09C1B910E03608BA2F");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch("http://www.mapquestapi.com/geocoding/v1/address?key=paBljA5O7dNn6iVSmZEQGDGQSfP0BWeK&location="+zipcode, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+
 function myFunction(zipcode) {
+    get_geocode(zipcode);
     var target = document.getElementById("weatherResults");
     target.innerHTML += `
     <table>
