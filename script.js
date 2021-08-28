4adadd0032161f76e2bd906a015f0874a8f5b220
Scott Breitenbach <scott.breitenbach@gmail.com>
2021-08-27 22:12:01 -0400

    getting the lat and long

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function get_lat_long(geocode_data)
+{
+    console.log(geocode_data.results[0].locations[0].latLng)
+}
+
 function get_geocode(zipcode) {
     var myHeaders = new Headers();
     myHeaders.append("Cookie", "JSESSIONID=061C62770675DB09C1B910E03608BA2F");
@@ -8,10 +13,18 @@ function get_geocode(zipcode) {
         redirect: 'follow'
     };
 
-    fetch("http://www.mapquestapi.com/geocoding/v1/address?key=paBljA5O7dNn6iVSmZEQGDGQSfP0BWeK&location="+zipcode, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+    fetch("http://www.mapquestapi.com/geocoding/v1/address?key=paBljA5O7dNn6iVSmZEQGDGQSfP0BWeK&location=" + zipcode, requestOptions)
+    .then(function(response){
+        // response is a json string
+       return response.json();// convert it to a pure JavaScript object
+   })
+   .then(function(data){
+        //Process Your data  
+        get_lat_long(data);
+   })
+   .catch(function(err) {
+       console.log(err);
+     });
 }
 
 
